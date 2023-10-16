68f504c9b288f8421e8de9cf40d927e5c8570248
viaheather <viaheatherr@gmail.com>
2023-10-15 17:37:22 -0700

    revised

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE departments (
 CREATE TABLE roles (
   role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   job_title VARCHAR(30) NOT NULL,
-  department_name INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
   salary INT NOT NULL,
   FOREIGN KEY (department_name) REFERENCES departments(department_name)
 );
@@ -24,7 +24,7 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   job_title VARCHAR(30) NOT NULL,
-  department_name INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
   salary INT NOT NULL,
   manager_id INT,
   FOREIGN KEY (department_name) REFERENCES departments(department_name),
